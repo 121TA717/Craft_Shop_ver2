8ef6fbce8b493f05802553e02810ef18e773aad1
121TA717 <m13071219@gmail.com>
2022-08-19 13:49:24 +0000

    Add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220807143257) do
+ActiveRecord::Schema.define(version: 20220819134424) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",        null: false
@@ -33,8 +33,11 @@ ActiveRecord::Schema.define(version: 20220807143257) do
   end
 
   create_table "cart_items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "amount",      null: false
+    t.integer  "item_id",     null: false
+    t.integer  "customer_id", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "customers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -58,13 +61,42 @@ ActiveRecord::Schema.define(version: 20220807143257) do
   end
 
   create_table "favorits", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "item_id",     null: false
+    t.integer  "customer_id", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name",                       null: false
+    t.text     "introduction", limit: 65535, null: false
+    t.integer  "price",                      null: false
+    t.boolean  "is_active",                  null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  create_table "order_details", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "order_id",      null: false
+    t.integer  "item_id",       null: false
+    t.integer  "price",         null: false
+    t.integer  "amount",        null: false
+    t.integer  "making_status", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  create_table "orders", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "postal_code",    null: false
+    t.string   "address",        null: false
+    t.string   "name",           null: false
+    t.integer  "shipping_cost",  null: false
+    t.integer  "total_payment",  null: false
+    t.integer  "payment_method", null: false
+    t.integer  "status",         null: false
+    t.integer  "customer_id",    null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
 end
