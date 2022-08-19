7e1188b265448e08d4e7c01daae7dff68941091d
121TA717 <m13071219@gmail.com>
2022-08-19 15:48:07 +0000

    Update views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'public/homes#top'
 # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
+
 
 # 管理者用
 # URL /admin/sign_in ...
